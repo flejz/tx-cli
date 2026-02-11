b4ea16a0ea5b1106ecd94b5fd205e49bec7bf7be
flejz <flejz@protonmail.com>
2026-02-11 22:17:08 +0100

    feat: consider up to 4 decimal values on data deserialization

--- src/model/transaction.rs
+++ src/model/transaction.rs
@@ -1,4 +1,4 @@
-use rust_decimal::Decimal;
+use rust_decimal::{Decimal, RoundingStrategy};
 
 #[derive(Debug, PartialEq, serde::Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -10,10 +10,19 @@ pub enum TransactionType {
     Chargeback,
 }
 
+fn deserialize_amount_4_dp<'de, D>(deserializer: D) -> Result<Decimal, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let value = <Decimal as serde::Deserialize>::deserialize(deserializer)?;
+    Ok(value.round_dp_with_strategy(4, RoundingStrategy::ToZero))
+}
+
 #[derive(Debug, serde::Deserialize)]
 pub struct Transaction {
     pub r#type: TransactionType,
     pub client: u16,
     pub tx: u32,
+    #[serde(deserialize_with = "deserialize_amount_4_dp")]
     pub amount: Decimal,
 }
